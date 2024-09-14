5a2437026605977d8a7868fd9210f2f1d12a1192
BhavinP2000 <bhavinskpatel2000@gmail.com>
2024-09-14 12:40:20 +0530

    dart doc

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,18 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:helmsman/Frontend/Src/splash_sscr.dart';
 
+/// The main entry point of the application.
+///
+/// Initializes Flutter bindings and `GetStorage` for local storage,
+/// then runs the `MyApp` widget.
+///
+/// - `WidgetsFlutterBinding.ensureInitialized()`: Prepares the framework.
+/// - `GetStorage.init()`: Initializes persistent storage.
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 
